0d9e795f702ccbc56f374318db15d397c257bfe1
Joel Encarnacion <joelencarnacion07@gmail.com>
2025-09-18 18:28:57 -0400

    chore: add montserrat font

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -3,7 +3,11 @@ import { Button } from '@/components/ui/button';
 export const HomePage = () => {
 	return (
 		<div>
-			HomePage
+			<h1 className='font-montserrat'>home</h1>
+			<h1 className='font-montserrat font-thin'>home</h1>
+			<h1 className='font-montserrat font-normal'>home</h1>
+			<h1 className='font-montserrat font-bold'>home</h1>
+
 			<Button variant='outline'>Hello</Button>
 		</div>
 	);
