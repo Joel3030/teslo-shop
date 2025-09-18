0bf4c8c2083cd7343950b99a5bf4fecb175dbadc
Joel Encarnacion <joelencarnacion07@gmail.com>
2025-09-18 18:22:24 -0400

    feat: Add router and main page

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter, Navigate } from 'react-router';
 import { ShopLayout } from './shop/layouts/ShopLayout';
+import { GenderPage } from './shop/pages/gender/GenderPage';
 import { HomePage } from './shop/pages/home/HomePage';
 import { ProductPage } from './shop/pages/product/ProductPage';
-import { GenderPage } from './shop/pages/gender/GenderPage';
 
 import { LoginPage } from './auth/pages/login/LoginPage';
 import { RegisterPage } from './auth/pages/register/RegisterPage';
 
-import { DashboardPage } from './admin/pages/dashboard/DashboardPAge';
-import { AdminProductsPage } from './admin/pages/products/AdminProductsPage';
-import { AdminProductPage } from './admin/pages/product/AdminProductPage';
 import { lazy } from 'react';
+import { DashboardPage } from './admin/pages/dashboard/DashboardPage';
+import { AdminProductPage } from './admin/pages/product/AdminProductPage';
+import { AdminProductsPage } from './admin/pages/products/AdminProductsPage';
 
 const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'));
 const AdminLayout = lazy(() => import('./admin/layouts/AdminLayout'));
